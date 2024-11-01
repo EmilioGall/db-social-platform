ea9f7bcb6d376592c979684997d36733ce28893c
Emilio Gallo <emilio.gallo17@gmail.com>
2024-11-01 19:40:08 +0100

    feat: added query_2 - queries.sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,10 @@
--- 1. Seleziona tutti gli utenti e calcolane l'età
+-- 1. Select all users and their age
 SELECT username, 
        YEAR(CURDATE()) - YEAR(birthdate) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(birthdate, '%m%d')) AS age
-FROM users;
+FROM `users`;
+
+-- 2. Select all posts without likes
+SELECT *
+FROM `posts`
+LEFT JOIN `likes` ON posts.id = likes.post_id
+WHERE likes.post_id IS NULL;
