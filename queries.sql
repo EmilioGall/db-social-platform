02953ff4126b92b0919a637a06248e0a746f8997
Emilio Gallo <emilio.gallo17@gmail.com>
2024-11-01 19:49:25 +0100

    feat: added query_3 - queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,5 @@
 -- 1. Select all users and their age
-SELECT username, 
-       YEAR(CURDATE()) - YEAR(birthdate) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(birthdate, '%m%d')) AS age
+SELECT username, YEAR(CURDATE()) - YEAR(birthdate) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(birthdate, '%m%d')) AS age
 FROM `users`;
 
 -- 2. Select all posts without likes
@@ -8,3 +7,10 @@ SELECT *
 FROM `posts`
 LEFT JOIN `likes` ON posts.id = likes.post_id
 WHERE likes.post_id IS NULL;
+
+-- 3. Count number of likes for each post
+SELECT posts.id AS post_id, COUNT(likes.post_id) AS like_count
+FROM posts
+LEFT JOIN likes ON posts.id = likes.post_id
+GROUP BY posts.id;
+
